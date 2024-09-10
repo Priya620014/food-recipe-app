aa55d1ab356dd2c574aad3ad1365ab6f4ff32b5f
Priyanshi <priyanshiraj347@gmail.com>
2024-09-10 16:05:29 +0530

    Bcrypt Update

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 // BACKEND_URL = "http://localhost:2000";
 
-const BACKEND_URL = "https://recipesharingcommunity.onrender.com";
+const BACKEND_URL = "http://e9e546e093db:3000/";
 
 export { BACKEND_URL };
