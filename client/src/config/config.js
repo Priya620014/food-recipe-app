35e756eea436530b3323c86bb7febb506037ae29
Priyanshi <priyanshiraj347@gmail.com>
2024-09-30 06:48:04 +0530

    New Update Style For this

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
-const BACKEND_URL = "http://localhost:2000";
+// const BACKEND_URL = "http://localhost:2000";
 
-// const BACKEND_URL = "https://recipeserver-96hb.onrender.com";
+const BACKEND_URL = "https://recipeserver-96hb.onrender.com";
 
 export { BACKEND_URL };
